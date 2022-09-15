d15981c1a65abc846b46458b58cac3452c1e18f1
Will Robertson <williamdrobertson42@gmail.com>
2022-09-15 20:26:29 +1000

    Add addRecipe request handler

--- src/controllers/recipesController.ts
+++ src/controllers/recipesController.ts
@@ -1,10 +1,11 @@
 import { RequestHandler } from 'express';
 import pool from '../db/database';
+import timeStringToMinutes from '../util/timeStringToMinutes';
 
 interface RecipeContents {
   name: string;
   source: string;
-  time: number;
+  time: string | number;
   serves: string;
   ingredients: Array<string>;
   method: Array<string>;
@@ -34,4 +35,35 @@ const getRecipes: RequestHandler = async (req, res) => {
   }
 };
 
-export { getRecipes };
+const addRecipe: RequestHandler = async (req, res) => {
+  if (!req.session) {
+    return res.status(404).json({ error: 'No session found' });
+  }
+
+  const userId = req.session.userId;
+  let newRecipe: RecipeContents = req.body;
+
+  if (!userId || !newRecipe) {
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+
+  // parse time string to num
+  newRecipe.time = timeStringToMinutes(newRecipe.time);
+  console.log(newRecipe);
+
+  try {
+    const client = await pool.connect();
+    const values = [userId, newRecipe];
+    const query = 'INSERT INTO recipes(user_id, contents) VALUES ($1, $2)';
+
+    const dbResponse = await client.query(query, values);
+
+    client.release();
+    return res.status(201).json({ message: 'Recipe added' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+export { getRecipes, addRecipe };
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { getRecipes } from '../controllers/recipesController';
+import { getRecipes, addRecipe } from '../controllers/recipesController';
 const router = express.Router();
 
 router.get('/recipes', getRecipes);
+router.post('/recipes', addRecipe);
 
 export default router;
